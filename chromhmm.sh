a692df45c5e1a6e3719873e98fe1a58435edce9f
Shbhatia <sbhatia@u.nus.edu>
2017-02-17 00:52:02 +0800

    Update chromhmm.sh

--- chromhmm.sh
+++ chromhmm.sh
@@ -1,1 +1,2 @@
 #Binarize Bam files
+java -jar ChromHMM.jar BinarizeBam /home/sb/genome_data/GRCh38/hg38.chrom.sizes /home/sb/grace/chromhmm/inputbamdir /home/sb/grace/chromhmm/cellmarkfiletable /home/sb/grace/chromhmm/outputbinarydir
