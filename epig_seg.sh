e718e346da9f78e19a9151ed6e8c848f0b4565a5
Shbhatia <sbhatia@u.nus.edu>
2017-02-22 18:03:55 +0800

    Update epig_seg.sh

--- epig_seg.sh
+++ epig_seg.sh
@@ -5,9 +5,10 @@ sed '/_/d' ./regions2.bed > regions_new.bed
 sed '/M/d' ./regions_new.bed > regions.bed
 #Create a counts matrix
 /home/sb/programfiles/epicseg.R getcounts --regions /home/sb/grace/epicseg/regions.bed --mark ctcf:/home/sb/grace/chromhmm/inputdir/mcf7_ctcf.bam --mark p300:/home/sb/grace/chromhmm/inputdir/mcf7_p300.bam --mark h3k27ac:/home/sb/grace/chromhmm/inputdir/mcf7_h3k27ac.bam --mark h3k4me1:/home/sb/grace/chromhmm/inputdir/mcf7_h3k4me1.bam --mark h3k4me2:/home/sb/grace/chromhmm/inputdir/mcf7_h3k4me2.bam --mark h3k36me3:/home/sb/grace/chromhmm/inputdir/mcf7_h3k36me3.bam --mark h3k27me3:/home/sb/grace/chromhmm/inputdir/mcf7_h3k27me3.bam --mark h3k9ac:/home/sb/grace/chromhmm/inputdir/mcf7_h3k9ac.bam --mark dnase:/home/sb/grace/chromhmm/inputdir/mcf7_dnase.bam --target /home/sb/grace/epicseg/epic_out1.txt --nthreads 15
-/home/sb/programfiles/epicseg.R getcounts --regions /home/sb/grace/epicseg/grace_peaks/merge_regions.bed --mark h3k27ac:/home/sb/grace/chromhmm/inputdir/mcf7_h3k27ac.bam --mark h3k4me1:/home/sb/grace/chromhmm/inputdir/mcf7_h3k4me1.bam --mark h3k36me3:/home/sb/grace/chromhmm/inputdir/mcf7_h3k36me3.bam --mark h3k27me3:/home/sb/grace/chromhmm/inputdir/mcf7_h3k27me3.bam --mark h3k9ac:/home/sb/grace/chromhmm/inputdir/mcf7_h3k9ac.bam --mark dnase:/home/sb/grace/chromhmm/inputdir/mcf7_dnase.bam --target /home/sb/grace/epicseg/epic_out1.txt --nthreads 15
+Rscript epicseg.R getcounts --regions /home/sb/epicseg/grace_peaks/regions.bed --mark h3k27ac:/home/sb/inputdir/mcf7_h3k27ac.bam --mark h3k4me1:/home/sb/inputdir/mcf7_h3k4me1.bam --mark h3k36me3:/home/sb/inputdir/mcf7_h3k36me3.bam --mark h3k27me3:/home/sb/inputdir/mcf7_h3k27me3.bam --mark h3k9ac:/home/sb/inputdir/mcf7_h3k9ac.bam --target /home/sb/epicseg/epic_out2.txt --nthreads 15
 
 #Run segmentation - on server
 /home/sb/programfiles/epicseg.R segment --counts /home/sb/grace/epicseg/epic_out1.txt --regions /home/sb/grace/epicseg/epic_out1_refined_regions.bed --nstates 8 --nthreads 15 --outdir /home/sb/grace/epicseg
 #Run segmentation - locally
 Rscript epicseg.R segment --counts ./epic_out1.txt --regions ./epic_out1_refined_regions.bed --nstates 8 --nthreads 15 --outdir ./ 
+Rscript epicseg.R segment --counts ./epic_out2.txt --regions ./epic_out2_refined_regions.bed --nstates 8 --nthreads 15 --outdir ./ 
