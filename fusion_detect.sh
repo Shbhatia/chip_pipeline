7ccb7cff0824742e5e856ba676a4c50cb40ab675
Shbhatia <sbhatia@u.nus.edu>
2017-02-24 10:31:35 +0800

    Update fusion_detect.sh

--- fusion_detect.sh
+++ fusion_detect.sh
@@ -1,1 +1,16 @@
+#run STAR
+STAR --genomeDir hg38_index \
+--readFilesIn ${left_fq_filename} ${right_fq_filename} \ --twopassMode Basic \
+--outReadsUnmapped None \
+--chimSegmentMin 20 \
+--chimJunctionOverhangMin 12 \ --alignSJDBoverhangMin 10 \
+--alignMatesGapMax 200000 \
+--alignIntronMax 200000 \
+--chimSegmentReadGapMax parameter 3 \ --alignSJstitchMismatchNmax 5 -1 5 5 \
+--runThreadN 16 \
+--outSAMtype BAM SortedByCoordinate
 
+#run STAR-Fusion
+STAR-Fusion --genome_lib_dir /path/to/your/CTAT_resource_lib \ -J Chimeric.out.junction \
+--CPU 15
+--output_dir star_fusion_outdir
