6ac0665e1a65e89cc41675988e1286b208236f5e
Shbhatia <sbhatia@u.nus.edu>
2017-02-27 18:16:58 +0800

    Update deeptools_heatmap.sh

--- deeptools_heatmap.sh
+++ deeptools_heatmap.sh
@@ -3,5 +3,5 @@ computeMatrix reference-point -S /home/sb/grace/chip/cdts-wh.genomics.cn/F16FTSA
 #computematrix with homerout+epicseg (4-state; state 3)
 computeMatrix reference-point -S /home/sb/grace/chip/cdts-wh.genomics.cn/F16FTSAPHT1209_HUMbgsC/Clean/Ac-1/star_output/h3k27ac0h_seqdepthnorm.bw -R /home/sb/grace/epicseg/homer/state3.bed --outFileName /home/sb/grace/epicseg/homer/heatmap_homer.gz --referencePoint center -b 5000 -a 5000 --blackListFileName /home/sb/genome_data/GRCh38/hg38.blacklist.bed --numberOfProcessors 20
 #heatmap
-plotHeatMap --matrixfile /home/sb/grace/epicseg/4state_q0.01/matrix1 --outFileName heatmap_h3k27ac_enh.pdf 
-plotHeatMap --matrixfile /home/sb/grace/epicseg/homer/heatmap_homer.gz --outFileName /home/sb/grace/epicseg/homer/heatmap_h3k27ac_enh.pdf 
+plotHeatmap --matrixFile /home/sb/grace/epicseg/4state_q0.01/matrix1 --outFileName heatmap_h3k27ac_enh.pdf 
+plotHeatmap --matrixFile /home/sb/grace/epicseg/homer/heatmap_homer.gz --outFileName /home/sb/grace/epicseg/homer/heatmap_h3k27ac_enh.pdf 
