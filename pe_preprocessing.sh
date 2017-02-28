e19b0693971708505311e90127018c957b68937a
Shbhatia <sbhatia@u.nus.edu>
2017-02-28 17:50:23 +0800

    Update pe_preprocessing.sh

--- pe_preprocessing.sh
+++ pe_preprocessing.sh
@@ -231,6 +231,10 @@ findPeaks /home/sb/grace/homer/h3k27ac-4h-ChIP-Seq/ -style histone -o auto -i /h
 findPeaks /home/sb/grace/homer/h3k27ac-24h-ChIP-Seq/ -style histone -o auto -i /home/sb/grace/homer/Control-24h-ChIP-Seq/
 findPeaks /home/sb/grace/homer/h3k4me1-0h-ChIP-Seq/ -style histone -o auto -i /home/sb/grace/homer/Control-0h-ChIP-Seq/
 findPeaks /home/sb/grace/homer/h3k4me1-4h-ChIP-Seq/ -style histone -o auto -i /home/sb/grace/homer/Control-4h-ChIP-Seq/
+findPeaks /home/sb/grace/homer/h3k4me1-24h-ChIP-Seq/ -style histone -o auto -i /home/sb/grace/homer/Control-24h-ChIP-Seq/
+findPeaks /home/sb/grace/homer/h3k27ac-0h-ChIP-Seq/ -style super -o auto -typical typical.txt -i /home/sb/grace/homer/Control-0h-ChIP-Seq/
+findPeaks /home/sb/grace/homer/h3k27ac-4h-ChIP-Seq/ -style super -typical typical.txt -L 0 -o auto -i /home/sb/grace/homer/Control-4h-ChIP-Seq/
+findPeaks /home/sb/grace/homer/h3k27ac-24h-ChIP-Seq/ -style super -typical /home/sb/grace/homer/h3k27ac-24h-ChIP-Seq/typical.txt -L 0 -o auto -i /home/sb/grace/homer/Control-24h-ChIP-Seq/
 
 #homer-output processing bed file
 cut -f2,3,4,5 regions.txt | grep -v '^#' - | awk '{OFS="\t"; if ($4=="+") {print} else {print $1,$3,$2,$4}}' - > h3k27ac_homer.bed
