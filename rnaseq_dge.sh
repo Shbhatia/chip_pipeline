69a1d140dc37e1719d0b9532ba351978bd1bb7a4
Shbhatia <sbhatia@u.nus.edu>
2017-03-07 17:04:12 +0800

    Update rnaseq_dge.sh

--- rnaseq_dge.sh
+++ rnaseq_dge.sh
@@ -1,1 +1,11 @@
+#Alignment using STAR
 
+
+#using htseq-count and DESeq2
+#htseq-count SiC
+htseq-count -f bam -r pos --stranded=no /home/sb/star_output/mc_star_output/Aligned.out.bam /home/sb/genome_data/GRCh38/annotation/Homo_sapiens.GRCh38.77.gtf
+#htseq-count SiT
+htseq-count -f bam -r pos --stranded=no /home/sb/star_output/mt_star_output/Aligned.out.bam /home/sb/genome_data/GRCh38/annotation/Homo_sapiens.GRCh38.77.gtf
+#metadata
+
+#
