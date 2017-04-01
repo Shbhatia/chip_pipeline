2c48315b88515f89efed10a31da189bdf78b8d9a
Shbhatia <sbhatia@u.nus.edu>
2017-04-01 21:20:19 +0800

    Update miso.sh

--- miso.sh
+++ miso.sh
@@ -1,3 +1,1 @@
 miso —-run /home/sb/genome_data/GRCh38/annotation/gff/commonshortest/indexed_SE_events/ /home/sb/star_output/mc_star_ucsc/sorted.bam --output-dir /home/sb/miso/mc_out_pe/ —-read-len 101 —-paired-end 179.4 53.9
-
-miso --run /home/sb/genome_data/GRCh38/annotation/gff/commonshortest/indexed_SE_events/ /home/sb/star_output/mt_star_ucsc/sorted.bam --output-dir /home/sb/miso/mt_output1/ --read-len 101 
