b9b705e4d84fc6894c9fce22b39cd5b7d40c8ec8
Shbhatia <sbhatia@u.nus.edu>
2017-04-17 14:51:55 +0800

    Update counts_combined_se.sh

--- counts_combined_se.sh
+++ counts_combined_se.sh
@@ -1,2 +1,6 @@
 beads bed2gff combined_se.bed > combined_se.gff
 
+htseq-count -f bam -r pos --stranded=no /home/sb/grace/chip/cdts-wh.genomics.cn/F16FTSAPHT1209_HUMbgsC/Clean/Ac-1/star_output/sorted2.bam /home/sb/modified_se_galaxy.gff
+htseq-count -f bam -r pos --stranded=no /home/sb/grace/chip/cdts-wh.genomics.cn/F16FTSAPHT1209_HUMbgsC/Clean/Ac-2/star_output/sorted2.bam /home/sb/modified_se_galaxy.gff
+htseq-count -f bam -r pos --stranded=no /home/sb/grace/chip/cdts-wh.genomics.cn/F16FTSAPHT1209_HUMbgsC/Clean/Ac-3/star_output/sorted2.bam /home/sb/modified_se_galaxy.gff
+
