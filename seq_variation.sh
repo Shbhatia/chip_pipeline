414be1f6b280ec3af18ea6ffafda982aad379ffc
Shbhatia <sbhatia@u.nus.edu>
2017-04-26 18:58:13 +0800

    Update seq_variation.sh

--- seq_variation.sh
+++ seq_variation.sh
@@ -7,4 +7,23 @@ git clone https://github.com/lh3/bwa.git
 #create bwa index
 bwa index -a bwtsw /home/sb/genome_data/GRCh38/sequence/hg38.fa
 
-#alignment using bwa
+#data from GEO
+#rep-1
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX039/SRX039778/SRR096941/SRR096941.sra
+#rep-2
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX039/SRX039779/SRR096942/SRR096942.sra
+
+#exp-2
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX101/SRX101494/SRR353720/SRR353720.sra
+
+#convert to fastq
+fastq-dump --split-files SRR096941.sra
+fastq-dump --split-files SRR096942.sra
+fastq-dump --split-files SRR353720.sra
+
+#zip fastq files
+
+#combine replicates for each read pair
+cat SRR096941_1.fastq.gz SRR096942_1.fastq.gz > mcf7_1.fastq.gz
+cat SRR096941_2.fastq.gz SRR096942_2.fastq.gz > mcf7_2.fastq.gz
+
