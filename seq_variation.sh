1d54bba9ad081c390280160a91412bdca624851e
Shbhatia <sbhatia@u.nus.edu>
2017-05-07 19:11:07 +0800

    Update seq_variation.sh

--- seq_variation.sh
+++ seq_variation.sh
@@ -27,3 +27,14 @@ fastq-dump --split-files SRR353720.sra
 cat SRR096941_1.fastq.gz SRR096942_1.fastq.gz > mcf7_1.fastq.gz
 cat SRR096941_2.fastq.gz SRR096942_2.fastq.gz > mcf7_2.fastq.gz
 
+#bwa aln
+./bwa aln -t 20 /home/sb/genome_data/GRCh38/sequence/hg38.fa /home/sb/genome_seq_mcf7/ENCSR000AHE/mcf7_seq.fq.gz > mcf7_bwaout.sai
+#bwa samse
+/home/sb/programfiles/bwa/bwa samse /home/sb/genome_data/GRCh38/sequence/hg38.fa /home/sb/genome_seq_mcf7/mcf7_bwaout.sai /home/sb/genome_seq_mcf7/ENCSR000AHE/mcf7_seq.fq.gz mcf7_grace.sam
+
+#bwa mem
+/home/sb/programfiles/bwa/bwa mem -M -t 35 /home/sb/genome_data/GRCh38/sequence/hg38.fa /home/sb/genome_seq_mcf7/ENCSR000AHE/mcf7_seq.fq.gz > mcf7_grace_mem.sam
+# -M ensures compatibility with picard
+
+samtools view -Sb mcf7_grace.sam |samtools sort - mcf7_grace
+
