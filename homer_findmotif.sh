0e777fd7c333320b3aa90ef9610c3501931fb481
Shbhatia <sbhatia@u.nus.edu>
2017-05-29 10:15:33 +0800

    Update homer_findmotif.sh

--- homer_findmotif.sh
+++ homer_findmotif.sh
@@ -1,2 +1,2 @@
-
-findMotifsGenome.pl ERpeaks.txt hg38 ac_MotifOutput/ -size 200 -mask
+#unbiased motif search on regions with differential h3k27ac
+findMotifsGenome.pl /home/sb/new_grace_data/homer_3vs1.bed hg38 ac_MotifOutput/ -size given -mask -p 10
