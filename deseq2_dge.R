e3f4e33fe8315886657af25ea136ac43a9fb1c66
Shbhatia <sbhatia@u.nus.edu>
2017-05-30 14:48:02 +0800

    Update deseq2_dge.R

--- deseq2_dge.R
+++ deseq2_dge.R
@@ -4,8 +4,10 @@ biocLite("DESeq2")
 library(DESeq2)
 
 #mapping with STAR
-/home/sb/programfiles/STAR/source/STAR --runThreadN 18 --genomeDir /home/sb/genome_data/GRCh38/star_index_150 --readFilesIn /home/sb/shainan/rnaseq/ftpdata.novogene.cn:2300/C101HW17
-010347/Hep3B_SiC_K_1.fq /home/sb/shainan/rnaseq/ftpdata.novogene.cn:2300/C101HW17010347/Hep3B_SiC_K_2.fq --outFileNamePrefix /home/sb/shainan/rnaseq/staroutput/hep3b_sic/
+/home/sb/programfiles/STAR/source/STAR --runThreadN 18 --genomeDir /home/sb/genome_data/GRCh38/star_index_150 --readFilesIn /home/sb/mcf10_rnaseq/ /home/sb/shainan/rnaseq/ftpdata.novogene.cn:2300/C101HW17010347/Hep3B_SiC_K_2.fq --outFileNamePrefix /home/sb/shainan/rnaseq/staroutput/hep3b_sic/
+
+/home/sb/programfiles/STAR/source/STAR   --runThreadN 18   --genomeDir /home/sb/genome_data/GRCh38/star_index_100_ucsc   --readFilesIn /home/sb/mcf10a_rnaseq/10a/sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_1.fq  /home/sb/mcf10a_rnaseq/10a/sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_2.fq    --outFileNamePrefix /home/sb/star_output/10a_sic_star_ucsc/
+/home/sb/programfiles/STAR/source/STAR   --runThreadN 18   --genomeDir /home/sb/genome_data/GRCh38/star_index_100_ucsc   --readFilesIn /home/sb/mcf10a_rnaseq/10a/sit/FCC57BPACXX-WHHUMhahEAABRAAPEI-47_1.fq /home/sb/mcf10a_rnaseq/10a/sit/FCC57BPACXX-WHHUMhahEAABRAAPEI-47_2.fq  --outFileNamePrefix /home/sb/star_output/10a_sit_star_ucsc/
 
 #samtools to get sorted bam file
 samtools view -bS Aligned.out.sam > Aligned.out.bam
@@ -13,18 +15,18 @@ samtools sort Aligned.out.bam > sorted.bam
 samtools index -b sorted.bam 
 
 #Rsubread featurecounts
-data<-featureCounts(c("/home/sb/shainan/rnaseq/staroutput/hep3b_sic/sorted.bam", "/home/sb/shainan/rnaseq/staroutput/hep3b_sik/sorted.bam"),
-annot.ext="/home/sb/genome_data/GRCh38/annotation/Homo_sapiens.GRCh38.77.gtf",
+data<-featureCounts(c("/home/sb/star_output/mc_star_ucsc/mc_aligned_sortedbyc.bam", "/home/sb/star_output/mt_star_ucsc/mt_aligned_sortedbyc.bam", "/home/sb/star_output/10a_sic_star_ucsc/sorted.bam", "/home/sb/star_output/10a_sit_star_ucsc/sorted.bam"),
+annot.ext="/home/sb/genome_data/GRCh38/annotation/ucsc/ensGene1.gtf",
 isGTFAnnotationFile=TRUE,
 minMQS=10,
 strandSpecific=0,
 isPairedEnd=TRUE,
-nthreads=15,
-GTF.attrType="gene_name"
+nthreads=20,
+GTF.attrType="transcript_id"
 )
 
 counts = data[[1]]
-colabel <- read.csv("/home/sb/colnames.csv")
+colabel <- read.csv("/home/sb/h2az/h2az_colnames.csv")
 samp2 <- colabel[,-1]
 rownames(samp2) <- colabel[,1]
 
