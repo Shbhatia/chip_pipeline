a2bbd4b32654f3cc5cc3acd4f1cdee6c0f9e02bb
Shbhatia <sbhatia@u.nus.edu>
2017-05-31 15:22:04 +0800

    Update dexseq.sh

--- dexseq.sh
+++ dexseq.sh
@@ -5,6 +5,7 @@ python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_p
 samtools sort -n Aligned.out.bam > sorted_n.bam --threads 20
 
 #created bin intervals
-python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py /home/sb/Homo_sapiens_DEXSeq.chr.gff /home/sb/star_output/10a_sic_star_ucsc/sorted_n.bam /home/sb/dexseq_untreated1.txt -p yes -f bam
-
-
+python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py /home/sb/Homo_sapiens_DEXSeq.chr.gff /home/sb/star_output/10a_sic/sorted_n.bam /home/sb/dexseq_untreated1.txt -p yes -f bam
+python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py /home/sb/Homo_sapiens_DEXSeq.chr.gff /home/sb/star_output/10a_sit/sorted_n.bam /home/sb/dexseq_treated1.txt -p yes -f bam
+python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py /home/sb/Homo_sapiens_DEXSeq.chr.gff /home/sb/star_output/mc_star_output/sorted_n.bam /home/sb/dexseq_untreated2.txt -p yes -f bam
+python /home/sb/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py /home/sb/Homo_sapiens_DEXSeq.chr.gff /home/sb/star_output/mt_star_output/sorted_n.bam /home/sb/dexseq_treated2.txt -p yes -f bam
