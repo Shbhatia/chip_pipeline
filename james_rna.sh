1cfa2f68ef8759456e758d1083fdb1507517a70d
Shbhatia <sbhatia@u.nus.edu>
2017-06-02 15:50:25 +0800

    Create james_rna.sh

---
+++ james_rna.sh
@@ -0,0 +1,2 @@
+/home/sb/programfiles/STAR/source/STAR --runThreadN 18 --genomeDir /home/sb/genome_data/GRCh38/star_index_150 --readFilesIn /home/sb/james/raw_data/S10A_siC_1.fq.gz /home/sb/james/raw_data/S10A_siC_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/sb/james/star/sic
+/home/sb/programfiles/STAR/source/STAR --runThreadN 18 --genomeDir /home/sb/genome_data/GRCh38/star_index_150 --readFilesIn /home/sb/james/raw_data/S10A_siT_1.fq.gz /home/sb/james/raw_data/S10A_siT_2.fq.gz --readFilesCommand zcat --outFileNamePrefix /home/sb/james/star/sit
