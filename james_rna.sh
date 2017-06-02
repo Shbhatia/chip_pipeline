826687184798f78b5f82444cafeb9aef3384900c
Shbhatia <sbhatia@u.nus.edu>
2017-06-02 17:09:53 +0800

    Update james_rna.sh

--- james_rna.sh
+++ james_rna.sh
@@ -8,3 +8,28 @@ samtools sort Aligned.out.bam > sorted.bam
 samtools index -b sorted.bam
 
 #Rsubread
+data<-featureCounts(c("/home/sb/james/star/sic/10a_sic_sorted.bam", "/home/sb/james/star/sit/10a_sit_sorted.bam"),
+annot.ext="/home/sb/genome_data/GRCh38/annotation/Homo_sapiens.GRCh38.77.gtf",
+isGTFAnnotationFile=TRUE,
+minMQS=10,
+strandSpecific=0,
+isPairedEnd=TRUE,
+nthreads=20,
+GTF.attrType="gene_name"
+)
+
+counts = data[[1]]
+colabel <- read.csv("/home/sb/james/james_colnames.csv")
+samp2 <- colabel[,-1]
+rownames(samp2) <- colabel[,1]
+
+colnames(counts) <- rownames(samp2)
+
+library("DESeq2")
+dLRT <- DESeqDataSetFromMatrix(countData = counts, colData = samp2, design = ~ condition)
+dLRT <- DESeq(dLRT, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+dLRT_res <- results(dLRT)
+write.table(dLRT_res, "/home/sb/james/dLRT_res_RNASeq.txt")
+write.table(assay(dLRT_vsd), "/home/sb/james/dLRT_vsd.txt")
+grep -v "NA" dLRT_res_RNASeq.txt | perl -pe "s/\h/\t/g" - | perl -pe "s/\"//g" | cut -f1,3,6 > 10a_RNASeq_noNA.txt
