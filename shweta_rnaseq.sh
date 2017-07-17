19deca42c062e5e852ceec8acbf60f8e2f4eb160
Shbhatia <sbhatia@u.nus.edu>
2017-07-17 19:12:02 +0800

    Update shweta_rnaseq.sh

--- shweta_rnaseq.sh
+++ shweta_rnaseq.sh
@@ -14,11 +14,16 @@
 #edited prep_genome_lib.pl; set CPU to 15 and changed directory of STAR command to /home/sb/STAR/source/STAR
 perl ../FusionFilter/edit2_prep_genome_lib.pl --genome_fa ref_genome.fa --gtf ref_annot.gtf --blast_pairs blast_pairs.outfmt6.gz
 
-STAR-Fusion --genome_lib_dir /home/sb/programfiles/STAR-Fusion/GRCh38_gencode_v23_CTAT_lib/ \ 
+perl ./STAR-Fusion_new.pl --genome_lib_dir /home/sb/programfiles/STAR-Fusion_v0.7.0_FULL/GRCh38_gencode_v23_CTAT_lib/ \
 -J /home/sb/shweta/1/star_output/Chimeric.out.junction \
 --CPU 25 \
 --output_dir /home/sb/shweta/1/star_output/star_fusion_outdir/
 
+perl STAR-Fusion_new.pl --genome_lib_dir /home/sb/programfiles/STAR-Fusion_v0.7.0_FULL/GRCh38_gencode_v23_CTAT_lib/ \
+             --left_fq /home/sb/shweta/1/FCHGHHVBBXX-HKRDHUMgaxEAAARAAPEI-222_L1_1.fq.gz \
+             --right_fq /home/sb/shweta/1/FCHGHHVBBXX-HKRDHUMgaxEAAARAAPEI-222_L1_2.fq.gz \
+             --output_dir /home/sb/shweta/1/star_output/star_fusion_outdir
+             
 /home/sb/programfiles/STAR/source/STAR --genomeDir /home/sb/genome_data/GRCh38/star_index_100/ \
 --readFilesIn /home/sb/shweta/12_files/9/FCHCCY2BBXX-HKHUMmijEAAERAAPEI-207_1.fq.gz /home/sb/shweta/12_files/9/FCHCCY2BBXX-HKHUMmijEAAERAAPEI-207_2.fq.gz \
 --twopassMode Basic \
