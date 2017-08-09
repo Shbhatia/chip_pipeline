1d7d9653dda73aba8071fab3f45d4b2ff7e4e312
Shbhatia <sbhatia@u.nus.edu>
2017-08-09 15:07:00 +0800

    Create AML_samples.sh

---
+++ AML_samples.sh
@@ -0,0 +1,4 @@
+perl ./STAR-Fusion_new.pl --genome_lib_dir /home/sb/programfiles/STAR-Fusion-v1.0.0/GRCh38_gencode_v23_CTAT_lib/ \
+             --left_fq /home/sb/transsplicing/AML/AD164_RNA_1.fq.gz \
+             --right_fq /home/sb/transsplicing/AML/AD164_RNA_2.fq.gz \
+             --output_dir /home/sb/transsplicing/AML/FUSION/
