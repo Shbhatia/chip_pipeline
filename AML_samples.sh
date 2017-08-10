6f8b59d826763b7c9cac65d3b58a18bbfebaf127
Shbhatia <sbhatia@u.nus.edu>
2017-08-10 20:03:22 +0800

    Update AML_samples.sh

--- AML_samples.sh
+++ AML_samples.sh
@@ -2,3 +2,7 @@ perl ./STAR-Fusion_new.pl --genome_lib_dir /home/sb/programfiles/STAR-Fusion-v1.
              --left_fq /home/sb/transsplicing/AML/AD164_RNA_1.fq.gz \
              --right_fq /home/sb/transsplicing/AML/AD164_RNA_2.fq.gz \
              --output_dir /home/sb/transsplicing/AML/FUSION/
+perl ./STAR-Fusion_new.pl --genome_lib_dir /home/sb/programfiles/STAR-Fusion-v1.0.0/GRCh38_gencode_v23_CTAT_lib/ \
+             --left_fq /home/sb/transsplicing/AML/FT_AML_BM_2_S6_L006_R1_001.gz \
+             --right_fq /home/sb/transsplicing/AML/FT_AML_BM_2_S6_L006_R2_001.gz \
+             --output_dir /home/sb/transsplicing/AML/FUSION2/
