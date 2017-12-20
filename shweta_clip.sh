12d6c70896239b06be0a5fa10b1459083726a2ba
Shbhatia <sbhatia@u.nus.edu>
2017-12-20 13:18:17 +0800

    Update shweta_clip.sh

--- shweta_clip.sh
+++ shweta_clip.sh
@@ -1,1 +1,3 @@
+programfiles/STAR/source/STAR --runThreadN 20 --runMode genomeGenerate --genomeDir genome_data/GRCh38/star_index_noann --genomeFastaFiles genome_data/GRCh38/sequence/ensembl/GRCh38_r77.all.fa
 
+/home/sb/programfiles/STAR/source/STAR   --runThreadN 25   --genomeDir /home/sb/genome_data/GRCh38/star_index_noann  --readFilesIn /home/sb/shweta/CLIP/SRR034466.fastq --outFileNamePrefix /home/sb/shweta/CLIP/ --alignEndsType EndToEnd
