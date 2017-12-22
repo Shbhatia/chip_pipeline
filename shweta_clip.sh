a7181e697b0e7434724b0410123493fb83f6581b
Shbhatia <sbhatia@u.nus.edu>
2017-12-22 10:05:18 +0800

    Update shweta_clip.sh

--- shweta_clip.sh
+++ shweta_clip.sh
@@ -3,3 +3,6 @@ programfiles/STAR/source/STAR --runThreadN 20 --runMode genomeGenerate --genomeD
 /home/sb/programfiles/STAR/source/STAR   --runThreadN 25   --genomeDir /home/sb/genome_data/GRCh38/star_index_noann  --readFilesIn /home/sb/shweta/CLIP/SRR034466.fastq --outFileNamePrefix /home/sb/shweta/CLIP/SRR034466/ --alignEndsType EndToEnd
 
 /home/sb/programfiles/deepTools/bin/bamCoverage --bam /home/sb/shweta/CLIP/SRR034466/SRR034466_sorted.bam --binSize 10 --normalizeTo1x 3088286401 -o /home/sb/shweta/CLIP/SRR034466.bw
+/home/sb/programfiles/deepTools/bin/bamCoverage --bam /home/sb/shweta/CLIP/SRR034467/SRR034467_sorted.bam --binSize 10 --normalizeTo1x 3088286401 -o /home/sb/shweta/CLIP/SRR034467.bw
+/home/sb/programfiles/deepTools/bin/bamCoverage --bam /home/sb/shweta/CLIP/SRR034468/SRR034468_sorted.bam --binSize 10 --normalizeTo1x 3088286401 -o /home/sb/shweta/CLIP/SRR034468.bw
+/home/sb/programfiles/deepTools/bin/bamCoverage --bam /home/sb/shweta/CLIP/SRR034469/SRR034469_sorted.bam --binSize 10 --normalizeTo1x 3088286401 -o /home/sb/shweta/CLIP/SRR034469.bw
