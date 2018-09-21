c2a65232c4106681d3c1fc1ad4cdf52cce68dff4
Shbhatia <sbhatia@u.nus.edu>
2018-09-21 18:26:53 +0800

    Update James_shRNA_RNAseq.sh

--- James_shRNA_RNAseq.sh
+++ James_shRNA_RNAseq.sh
@@ -64,3 +64,27 @@ awk -F"\t" '{if ($2>1) print $0}' RNASeq_noNA.txt | perl -pe "s/\h/\t/g" - > upr
 awk -F"\t" '{if ($2<-1) print $0}' RNASeq_noNA.txt | perl -pe "s/\h/\t/g" - > downregulated_lgfc1.bed
 awk -F"\t" '{if ($3<0.05) print $0}' downregulated_lgfc1.bed | perl -pe "s/\h/\t/g" - > downregulated_lgfc1_p0.05.bed
 awk -F"\t" '{if ($3<0.05) print $0}' upregulated_lgfc1.bed | perl -pe "s/\h/\t/g" - > upregulated_lgfc1_p0.05.bed
+
+down=read.table("../17_feb/741_genes.txt")
+down=down$V1
+down.ix=which(rownames(x) %in% as.character(down))
+
+up=read.table("../UPPPP.txt")
+up=up$V1
+up.ix=which((rownames(x) %in% as.character(up)) & abs(x$log2FoldChange)>1 )
+
+postscript("volcano_TSSpeak_regions.ps", width= 50, height= 50)
+ smoothScatter(x$log2FoldChange,-log10(x$padj),xlab=expression('Log'[2]*' Fold Change'),ylab=expression('-Log'[10]*' P-values'))
+ p.ix= x$log2FoldChange>1 & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="goldenrod4", pch = 16)
+ p.ix= x$log2FoldChange<(-1) & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="darkblue", pch = 16)
+ abline(v=1, col ='grey',lty = 2);
+ abline(v=(-1), col ='grey',lty = 2);
+ abline(h=-log10(.05), col ='grey',lty = 2)
+ points(x$log2FoldChange[up.ix],-log10(x$padj[up.ix]),pch = 4,col="red")
+ points(x$log2FoldChange[down.ix],-log10(x$padj[down.ix]), pch = 4,col="red")
+
+
+dev.off()
+
