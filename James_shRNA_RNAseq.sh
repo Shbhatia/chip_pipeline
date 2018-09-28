d59b47cec38319a04be7c54184388eebe1369ec5
Shbhatia <sbhatia@u.nus.edu>
2018-09-28 18:04:12 +0800

    Update James_shRNA_RNAseq.sh

--- James_shRNA_RNAseq.sh
+++ James_shRNA_RNAseq.sh
@@ -65,28 +65,45 @@ awk -F"\t" '{if ($2<-1) print $0}' RNASeq_noNA.txt | perl -pe "s/\h/\t/g" - > do
 awk -F"\t" '{if ($3<0.05) print $0}' downregulated_lgfc1.bed | perl -pe "s/\h/\t/g" - > downregulated_lgfc1_p0.05.bed
 awk -F"\t" '{if ($3<0.05) print $0}' upregulated_lgfc1.bed | perl -pe "s/\h/\t/g" - > upregulated_lgfc1_p0.05.bed
 
-x=read.table("RNASeq_noNA.txt")
+x=read.table("dds_res_RNASeq.txt")
 library(graphics)
 down=read.table("../overlap/down_overlap_genenames.bed")
 down=down$V1
-down.ix=which(rownames(y) %in% as.character(down))
+down.ix=which(rownames(x) %in% as.character(down))
 
 up=read.table("../overlap/up_overlap_genenames.bed")
 up=up$V1
-up.ix=which((rownames(y) %in% as.character(up)) & abs(y$log2FoldChange)>1 )
+up.ix=which((rownames(x) %in% as.character(up)))
 
-postscript("volcano_shT12_1.ps", width= 50, height= 50)
- smoothScatter(y$log2FoldChange,-log10(x$padj),xlab=expression('Log'[2]*' Fold Change'),ylab=expression('-Log'[10]*' P-values'))
- p.ix= y$log2FoldChange>1 & y$padj<0.05
- points(y$log2FoldChange[p.ix],-log10(y$padj[p.ix]),col="goldenrod4", pch = 16)
- p.ix= y$log2FoldChange<(-1) & y$padj<0.05
- points(y$log2FoldChange[p.ix],-log10(y$padj[p.ix]),col="darkblue", pch = 16)
+pdf(file = "volcano_shT12_1a.pdf", width= 10, height= 10)
+ smoothScatter(x$log2FoldChange,-log10(x$padj),xlab=expression('Log'[2]*' Fold Change'),ylab=expression('-Log'[10]*' P-values'))
+ p.ix= x$log2FoldChange>1 & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="goldenrod4", pch = 16)
+ p.ix= x$log2FoldChange<(-1) & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="darkblue", pch = 16)
  abline(v=1, col ='grey',lty = 2);
  abline(v=(-1), col ='grey',lty = 2);
  abline(h=-log10(.05), col ='grey',lty = 2)
- points(y$log2FoldChange[up.ix],-log10(y$padj[up.ix]),pch = 4,col="red")
- points(y$log2FoldChange[down.ix],-log10(y$padj[down.ix]), pch = 4,col="red")
+ points(x$log2FoldChange[up.ix],-log10(x$padj[up.ix]),pch = 4,col="red")
+ points(x$log2FoldChange[down.ix],-log10(x$padj[down.ix]), pch = 4,col="red")
 
 
 dev.off()
+
+pdf(file = "volcano_shT12_2a.pdf", width= 10, height= 10)
+ smoothScatter(x$log2FoldChange,-log10(x$padj),xlab=expression('Log'[2]*' Fold Change'),ylab=expression('-Log'[10]*' P-values'))
+ p.ix= x$log2FoldChange>1 & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="goldenrod4", pch = 16)
+ p.ix= x$log2FoldChange<(-1) & x$padj<0.05
+ points(x$log2FoldChange[p.ix],-log10(x$padj[p.ix]),col="darkblue", pch = 16)
+ abline(v=1, col ='grey',lty = 2);
+ abline(v=(-1), col ='grey',lty = 2);
+ abline(h=-log10(.05), col ='grey',lty = 2)
+ points(x$log2FoldChange[up.ix],-log10(x$padj[up.ix]),pch = 4,col="red")
+ points(x$log2FoldChange[down.ix],-log10(x$padj[down.ix]), pch = 4,col="red")
+
+
+dev.off()
+
+
 
