441f012af336c437457423d17678578fc30e2038
Shbhatia <sbhatia@u.nus.edu>
2018-12-07 14:36:05 +0800

    Update DRIP.sh

--- DRIP.sh
+++ DRIP.sh
@@ -1,3 +1,5 @@
 programfiles/bwa/bwa index genome_data/GRCh38/sequence/GRCh38_r77.all.fa
 
 programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO.sam
+
+programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_IN_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_IN_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_IN.sam
