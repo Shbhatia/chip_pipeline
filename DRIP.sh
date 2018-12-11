1d97ab73d7c9b10c2fb5c9d6e05a49f71dc23b95
Shbhatia <sbhatia@u.nus.edu>
2018-12-11 18:59:41 +0800

    Update DRIP.sh

--- DRIP.sh
+++ DRIP.sh
@@ -1,3 +1,4 @@
+#Mapping with BWA
 programfiles/bwa/bwa index genome_data/GRCh38/sequence/GRCh38_r77.all.fa
 
 programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO.sam
@@ -12,9 +13,14 @@ programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa 
 
 programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIT_NUC.sam
 
-programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIC_NUC_IN_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIC_NUC_IN_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_NUC_IN.sam
+programfiles/bwa/bwa mem -M -t 30 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIC_NUC_IN_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIC_NUC_IN_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_NUC_IN.sam
 
-programfiles/bwa/bwa mem -M -t 25 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_IN_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_IN_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIT_NUC_IN.sam
+programfiles/bwa/bwa mem -M -t 30 genome_data/GRCh38/sequence/GRCh38_r77.all.fa /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_IN_1.fq.gz /home/sb/DRIP/C101HW18101721/raw_data/SIT_NUC_IN_2.fq.gz > /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIT_NUC_IN.sam
 
-#MarkDuplicates
+#MarkDuplicates - PICARD
 java -jar /home/sb/programfiles/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true METRICS_FILE=/home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_duplicates.txt INPUT=/home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_sorted.bam OUTPUT=/home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_nodup.bam
+java -jar /home/sb/programfiles/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true METRICS_FILE=/home/sb/DRIP/C101HW18101721/raw_data/SIC_CYTO_IN_duplicates.txt INPUT=/home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_IN_sorted.bam OUTPUT=/home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_IN_nodup.bam
+
+
+#create bw files - DEEPTOOLS
+/home/sb/programfiles/deepTools/bin/bamCoverage --bam /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/SIC_CYTO_sorted.bam --binSize 10 --normalizeTo1x 3088286401 -o /home/sb/DRIP/C101HW18101721/raw_data/bwaalign/bigwig/SIC_CYTO.bw
