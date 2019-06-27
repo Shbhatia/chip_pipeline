dc16843276ac8b3640e64c355f06115ecaf2f862
Shbhatia <sbhatia@u.nus.edu>
2019-06-27 11:41:01 +0800

    Update repenrich.sh

--- repenrich.sh
+++ repenrich.sh
@@ -1,1 +1,4 @@
 
+python programfiles/RepEnrich/RepEnrich_setup.py repenrich/hg38_repeatmasker_clean.txt genome_data/GRCh38/sequence/hg38.fa repenrich/setup_folder_hg38
+
+bowtie /home/sb/repenrich/GRCh38/ -p 30 -t -m 1 -S --max /home/sb/10A-rnaseq/10asic_multimap.fastq -1 /home/sb/10A-rnaseq/10a_sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_1.fq -2 /home/sb/10A-rnaseq/10a_sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_2.fq /home/sb/10A-rnaseq/10asic_unique.sam
