f3e6fa49f3d7d1b2f63ee23e4d141c02765f2e1d
Shbhatia <sbhatia@u.nus.edu>
2019-06-28 17:58:40 +0800

    Update repenrich.sh

--- repenrich.sh
+++ repenrich.sh
@@ -1,6 +1,6 @@
-
+#repenrich setup
 python programfiles/RepEnrich/RepEnrich_setup.py repenrich/hg38_repeatmasker_clean.txt genome_data/GRCh38/sequence/hg38.fa repenrich/setup_folder_hg38
-
+#bowtie
 bowtie /home/sb/repenrich/hg38/hg38 -p 20 -t -m 1 -S --chunkmbs 400 --max /home/sb/10A-rnaseq/10asic_multimap.fastq -1 /home/sb/10A-rnaseq/10a_sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_1.fq -2 /home/sb/10A-rnaseq/10a_sic/FCC57BPACXX-WHHUMhahEAAARAAPEI-46_2.fq /home/sb/10A-rnaseq/10asic_unique.sam
 bowtie /home/sb/repenrich/hg38/hg38 -p 20 -t -m 1 -S --chunkmbs 400 --max /home/sb/10A-rnaseq/10asit_multimap.fastq -1 /home/sb/10A-rnaseq/10a_sit/FCC57BPACXX-WHHUMhahEAABRAAPEI-47_1.fq -2 /home/sb/10A-rnaseq/10a_sit/FCC57BPACXX-WHHUMhahEAABRAAPEI-47_2.fq /home/sb/10A-rnaseq/10asit_unique.sam
 
@@ -62,4 +62,136 @@ bowtie /home/sb/repenrich/hg38/hg38 -p 20 -t -m 1 -S --chunkmbs 400 --max /home/
 #Time searching: 00:50:38
 #Overall time: 00:50:38
 
+#repenrich
+python /home/sb/programfiles/RepEnrich/RepEnrich.py /home/sb/repenrich/hg38_repeatmasker_clean.txt /home/sb/repenrich/10a_sic_repenrich 10a_sic /home/sb/repenrich/RepEnrich_setup_hg38 /home/sb/10A-rnaseq/10asic_multimap_1.fastq --fastqfile2 /home/sb/10A-rnaseq/10asic_multimap_1.fastq /home/sb/10A-rnaseq/10asic_unique.bam --cpus 25 --pairedend TRUE
+python /home/sb/programfiles/RepEnrich/RepEnrich.py /home/sb/repenrich/hg38_repeatmasker_clean.txt /home/sb/repenrich/10a_sit_repenrich 10a_sit /home/sb/repenrich/RepEnrich_setup_hg38 /home/sb/10A-rnaseq/10asit_multimap_1.fastq --fastqfile2 /home/sb/10A-rnaseq/10asit_multimap_1.fastq /home/sb/10A-rnaseq/10asit_unique.bam --cpus 10 --pairedend TRUE
+
+python /home/sb/programfiles/RepEnrich/RepEnrich.py /home/sb/repenrich/hg38_repeatmasker_clean.txt /home/sb/repenrich/mscv_sic_repenrich mscv_sic /home/sb/repenrich/RepEnrich_setup_hg38 /home/sb/10A-rnaseq/mscv_sic_multimap_1.fastq --fastqfile2 /home/sb/10A-rnaseq/mscv_sic_multimap_2.fastq /home/sb/10A-rnaseq/mscv_sic_unique.bam --cpus 16 --pairedend TRUE
+
+
+#edgeR
+# EdgeR example
+
+# Setup - Install and load edgeR
+source("http://bioconductor.org/biocLite.R")
+biocLite("edgeR")
+library('edgeR')
+
+# In the case of a pre-assembled file of the fraction count output do the following:
+# counts <- read.csv(file = "counts.csv")
+
+# In the case of seperate outputs, load the RepEnrich results - fraction counts
+young_r1 <- read.delim('young_r1_fraction_counts.txt', header=FALSE)
+young_r2 <- read.delim('young_r2_fraction_counts.txt', header=FALSE)
+young_r3 <- read.delim('young_r3_fraction_counts.txt', header=FALSE)
+old_r1 <- read.delim('old_r1_fraction_counts.txt', header=FALSE)
+old_r2 <- read.delim('old_r2_fraction_counts.txt', header=FALSE)
+old_r3 <- read.delim('old_r3_fraction_counts.txt', header=FALSE)
+v_old_r1 <- read.delim('veryold_r1_fraction_counts.txt', header=FALSE)
+v_old_r2 <- read.delim('veryold_r2_fraction_counts.txt', header=FALSE)
+v_old_r3 <- read.delim('veryold_r3_fraction_counts.txt', header=FALSE)
+
+#' Build a counts table
+counts <- data.frame(
+  row.names = young_r1[,1],
+  young_r1 = young_r1[,4], young_r2 = young_r2[,4], young_r3 = young_r3[,4],
+  old_r1 = old_r1[,4], old_r2 = old_r2[,4], old_r3 = old_r3[,4],
+  v_old_r1 = v_old_r1[,4], v_old_r2 = v_old_r2[,4], v_old_r3 = v_old_r3[,4]
+)
+
+# Build a meta data object. I am comparing young, old, and veryold mice.
+# I manually input the total mapping reads for each sample.
+# The total mapping reads are calculated using the bowtie logs:
+# # of reads processed - # reads that failed to align
+meta <- data.frame(
+	row.names=colnames(counts),
+	condition=c("young","young","young","old","old","old","veryold","veryold","veryold"),
+	libsize=c(24923593,28340805,21743712,16385707,26573335,28131649,34751164,37371774,28236419)
+)
+
+# Define the library size and conditions for the GLM
+libsize <- meta$libsize
+condition <- factor(meta$condition)
+design <- model.matrix(~0+condition)
+colnames(design) <- levels(meta$condition)
+
+# Build a DGE object for the GLM
+y <- DGEList(counts=counts, lib.size=libsize)
+
+# Normalize the data
+y <- calcNormFactors(y)
+y$samples
+plotMDS(y)
+
+# Estimate the variance
+y <- estimateGLMCommonDisp(y, design)
+y <- estimateGLMTrendedDisp(y, design)
+y <- estimateGLMTagwiseDisp(y, design)
+plotBCV(y)
+
+# Build an object to contain the normalized read abundance
+logcpm <- cpm(y, log=TRUE, lib.size=libsize)
+logcpm <- as.data.frame(logcpm)
+colnames(logcpm) <- factor(meta$condition)
+
+# Conduct fitting of the GLM
+yfit <- glmFit(y, design)
+
+# Initialize result matrices to contain the results of the GLM
+results <- matrix(nrow=dim(counts)[1],ncol=0)
+logfc <- matrix(nrow=dim(counts)[1],ncol=0)
+
+# Make the comparisons for the GLM
+my.contrasts <- makeContrasts(
+	veryold_old = veryold – old,
+	veryold_young = veryold – young,
+	old_young = old – young,
+	levels = design
+)
+
+# Define the contrasts used in the comparisons
+allcontrasts = c(
+	"veryold_old",
+	"veryold_young",
+	"old_young"
+)
+
+# Conduct a for loop that will do the fitting of the GLM for each comparison
+# Put the results into the results objects
+for(current_contrast in allcontrasts) {
+	lrt <- glmLRT(yfit, contrast=my.contrasts[,current_contrast])
+	plotSmear(lrt, de.tags=rownames(y))
+	title(current_contrast)
+	res <- topTags(lrt,n=dim(c)[1],sort.by="none")$table
+	colnames(res) <- paste(colnames(res),current_contrast,sep=".")
+	results <- cbind(results,res[,c(1,5)])
+	logfc <- cbind(logfc,res[c(1)])
+}
+
+# Add the repeat types back into the results.
+# We should still have the same order as the input data
+results$class <- young_r1[,2]
+results$type <- young_r1[,3]
+
+# Sort the results table by the logFC
+results <- results[with(results, order(-abs(logFC.old_young))), ]
+
+# Save the results
+write.table(results, 'results.txt', quote=FALSE, sep="\t")
+
+# Plot Fold Changes for repeat classes and types
+for(current_contrast in allcontrasts) {
+  logFC <- results[, paste0("logFC.", current_contrast)]
+  # Plot the repeat classes
+  classes <- with(results, reorder(class, -logFC, median))
+  par(mar=c(6,10,4,1))
+  boxplot(logFC ~ classes, data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log(Fold Change)", main=current_contrast)
+  abline(v=0)
+  # Plot the repeat types
+  types <- with(results, reorder(type, -logFC, median))
+  boxplot(logFC ~ types, data=results, outline=FALSE, horizontal=TRUE,
+          las=2, xlab="log(Fold Change)", main=current_contrast)
+  abline(v=0)
+}
 
